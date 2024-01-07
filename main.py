88372af2e5bfd9ab6dd76287e4405d3ac28bf4f0
Daniel-Burbridge-Developer <Daniel.John.Developer@gmail.com>
2024-01-07 22:01:18 +0800

    How on earth do I add an image??

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+from tkinter import Tk, BOTH, Canvas
+import tkinter.ttk as ttk
+from PIL import Image, ImageTk
+
 def main():
-    print("Hello World!")
+
+
+    gui = GUI()
+
+
+class GUI:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Artist Guessing Game")
+        self.root.geometry("1800x1200")
+        self.root.resizable(False, False)
+        self.create_startup_screen()
+        self.root.mainloop()
+
+    def create_startup_screen(self):
+        image_path = "Images/TS_Eras_Tour.jpg"
+        img = Image.open(image_path)
+        img = ImageTk.PhotoImage(img)
+
+        # Create a label to display the image
+        label = ttk.Label(self.root, image=img)
+        label.pack()
+
 
 if __name__ == "__main__":
     main()
+
+
+#HOW DO I PUT A IMAGE IN WHAT THE HELL
+
     
