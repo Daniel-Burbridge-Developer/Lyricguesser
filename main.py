346f445fe445b49633f89753ef2071fa1c8209b1
Daniel-Burbridge-Developer <Daniel.John.Developer@gmail.com>
2024-01-10 19:07:07 +0800

    Day 2

--- main.py
+++ main.py
@@ -8,9 +8,13 @@ def main():
 class GameEngine:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.width = 1280
+        self.height = 720
+        self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
         self.running = True
+        pygame.display.set_caption("Lyric Guesser")
+        self.background = pygame.image.load("Images/TS_Eras_Tour.jpg")
 
     def run(self):
         while self.running:
@@ -29,8 +33,17 @@ class GameEngine:
         pass
 
     def render(self):
-        self.screen.fill("purple")
+        self.screen.fill("white")
+        self.screen.blit(self.background, (0, 0))
+        text = self.create_text("Taylor Swift Lyric Guesser", ("gold"), "Arial", 72)
+        text_rect = text.get_rect(center=(self.width // 2 + 50, self.height - self.height + 100))
+        self.screen.blit(text, text_rect)
         pygame.display.flip()
+
+    def create_text(self, text, color, font, size):
+        font = pygame.font.SysFont(font, size)
+        text = font.render(text, True, color)
+        return text
 
 if __name__ == "__main__":
     main()
